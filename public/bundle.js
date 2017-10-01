21e049edf52c61719af55040be3e97c9aa883eea
eesur <sundar@eesur.com>
2017-10-01 16:16:45 +0100

    make img links relative

--- public/bundle.js
+++ public/bundle.js
@@ -702,121 +702,121 @@ assign(App.prototype, methods, proto);
 const data$3 = {
   content: [
     {
-      thumbnail: '/assets/img/ch-01-t.png',
+      thumbnail: 'assets/img/ch-01-t.png',
       thumbCaption: 'ITEM 001',
       title: 'Sample Item 001',
       subtitle: '2017 Autumn',
       images: [
         {
-          img: '/assets/img/ch-01.jpg',
+          img: 'assets/img/ch-01.jpg',
           caption: 'Charley Harper Illustration (just used as sample)'
         }
       ],
       copy: 'Charley Harper (August 4, 1922 – June 10, 2007) was a Cincinnati-based American Modernist artist. He was best known for his highly stylized wildlife prints, posters and book illustrations.'
     },
     {
-      thumbnail: '/assets/img/ch-02-t.png',
+      thumbnail: 'assets/img/ch-02-t.png',
       thumbCaption: 'ITEM 002',
       title: 'Sample Item 002',
       subtitle: '2017 Autumn',
       images: [
         {
-          img: '/assets/img/ch-02.jpg',
+          img: 'assets/img/ch-02.jpg',
           caption: 'Charley Harper Illustration (just used as sample)'
         },
         {
-          img: '/assets/img/ch-02b.jpg',
+          img: 'assets/img/ch-02b.jpg',
           caption: 'Charley Harper Illustration'
         }
       ],
       copy: 'Charley Harper (August 4, 1922 – June 10, 2007) was a Cincinnati-based American Modernist artist. He was best known for his highly stylized wildlife prints, posters and book illustrations.'
     },
     {
-      thumbnail: '/assets/img/ch-03-t.png',
+      thumbnail: 'assets/img/ch-03-t.png',
       thumbCaption: 'ITEM 003',
       title: 'Sample Item 003',
       subtitle: '2017 Autumn',
       images: [
         {
-          img: '/assets/img/ch-03.jpg',
+          img: 'assets/img/ch-03.jpg',
           caption: 'Charley Harper Illustration (just used as sample)'
         }
       ],
       copy: 'Charley Harper (August 4, 1922 – June 10, 2007) was a Cincinnati-based American Modernist artist. He was best known for his highly stylized wildlife prints, posters and book illustrations.'
     },
     {
-      thumbnail: '/assets/img/ch-04-t.png',
+      thumbnail: 'assets/img/ch-04-t.png',
       thumbCaption: 'ITEM 004',
       title: 'Sample Item 004',
       subtitle: '2017 Autumn',
       images: [
         {
-          img: '/assets/img/ch-04.jpg',
+          img: 'assets/img/ch-04.jpg',
           caption: 'Charley Harper Illustration (just used as sample)'
         }
       ],
       copy: 'Charley Harper (August 4, 1922 – June 10, 2007) was a Cincinnati-based American Modernist artist. He was best known for his highly stylized wildlife prints, posters and book illustrations.'
     },
     {
-      thumbnail: '/assets/img/ch-05-t.png',
+      thumbnail: 'assets/img/ch-05-t.png',
       thumbCaption: 'ITEM 005',
       title: 'Sample Item 005',
       subtitle: '2017 Autumn',
       images: [
         {
-          img: '/assets/img/ch-05.jpg',
+          img: 'assets/img/ch-05.jpg',
           caption: 'Charley Harper Illustration (just used as sample)'
         }
       ],
       copy: 'Charley Harper (August 4, 1922 – June 10, 2007) was a Cincinnati-based American Modernist artist. He was best known for his highly stylized wildlife prints, posters and book illustrations.'
     },
     {
-      thumbnail: '/assets/img/ch-06-t.png',
+      thumbnail: 'assets/img/ch-06-t.png',
       thumbCaption: 'ITEM 006',
       title: 'Sample Item 006',
       subtitle: '2017 Autumn',
       images: [
         {
-          img: '/assets/img/ch-06.jpg',
+          img: 'assets/img/ch-06.jpg',
           caption: 'Charley Harper Illustration (just used as sample)'
         }
       ],
       copy: 'Charley Harper (August 4, 1922 – June 10, 2007) was a Cincinnati-based American Modernist artist. He was best known for his highly stylized wildlife prints, posters and book illustrations.'
     },
     {
-      thumbnail: '/assets/img/ch-07-t.png',
+      thumbnail: 'assets/img/ch-07-t.png',
       thumbCaption: 'ITEM 007',
       title: 'Sample Item 007',
       subtitle: '2017 Autumn',
       images: [
         {
-          img: '/assets/img/ch-07.jpg',
+          img: 'assets/img/ch-07.jpg',
           caption: 'Charley Harper Illustration (just used as sample)'
         }
       ],
       copy: 'Charley Harper (August 4, 1922 – June 10, 2007) was a Cincinnati-based American Modernist artist. He was best known for his highly stylized wildlife prints, posters and book illustrations.'
     },
     {
-      thumbnail: '/assets/img/ch-08-t.png',
+      thumbnail: 'assets/img/ch-08-t.png',
       thumbCaption: 'ITEM 008',
       title: 'Sample Item 008',
       subtitle: '2017 Autumn',
       images: [
         {
-          img: '/assets/img/ch-08.jpg',
+          img: 'assets/img/ch-08.jpg',
           caption: 'Charley Harper Illustration (just used as sample)'
         }
       ],
       copy: 'Charley Harper (August 4, 1922 – June 10, 2007) was a Cincinnati-based American Modernist artist. He was best known for his highly stylized wildlife prints, posters and book illustrations.'
     },
     {
-      thumbnail: '/assets/img/ch-09-t.png',
+      thumbnail: 'assets/img/ch-09-t.png',
       thumbCaption: 'ITEM 009',
       title: 'Sample Item 009',
       subtitle: '2017 Autumn',
       images: [
         {
-          img: '/assets/img/ch-09.jpg',
+          img: 'assets/img/ch-09.jpg',
           caption: 'Charley Harper Illustration (just used as sample)'
         }
       ],
